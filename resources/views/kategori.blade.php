4c221dbdda4c89640fb4231a9c6f4c76ced15fb4
Irfan Pandu Pratama <irfanpanduutama@gmail.com>
2026-03-10 16:52:00 +0700

    praktikum5

---
+++ resources/views/kategori.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <title>Data kategori barang</title>
+    </head>
+    <body>
+        <h1>data kategori barang</h1>
+        <table border='1' cellpadding='2' cellspacing='0'>
+            <tr>
+                <th>ID</th>
+                <th>Kode kategori</th>
+                <th>nama kategori</th>
+            </tr>
+            @foreach ($data as $d)
+                <tr>
+                    <td>{{$d->kategori_id}}</td>
+                    <td>{{$d->kategori_kode}}</td>
+                    <td>{{$d->nama_kategori}}</td>
+                </tr>
+            @endforeach
+        </table>
+    </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,11 @@
 
 use App\Http\Controllers\LevelController;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\KategoriController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::get('/level', [LevelController::class, 'index']);
+Route::get('/kategori', [KategoriController::class, 'index']);
