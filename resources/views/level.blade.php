ab4f30c707aa2a527b6f0b5e4a45cb7ca7686068
Irfan Pandu Pratama <irfanpanduutama@gmail.com>
2026-03-10 16:22:50 +0700

    praktikum4

---
+++ resources/views/level.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <title>Data level pengguna</title>
+    </head>
+    <body>
+        <h1>data level pengguna</h1>
+        <table border='1' cellpadding='2' cellspacing='0'>
+            <tr>
+                <th>ID</th>
+                <th>Kode level</th>
+                <th>nama level</th>
+            </tr>
+            @foreach ($data as $d)
+                <tr>
+                    <td>{{$d->level_id}}</td>
+                    <td>{{$d->level_kode}}</td>
+                    <td>{{$d->level_nama}}</td>
+                </tr>
+            @endforeach
+        </table>
+    </body>
+</html>
